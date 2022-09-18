13a838049cadd6acf8e64c820e877b8429db7648
code5753 <code.57x53@gmail.com>
2022-09-18 14:34:45 +0900

    feat: init project

---
+++ src/main/java/com/wfc/blogadvanced/BlogAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.wfc.blogadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAdvancedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wfc/blogadvanced/BlogAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wfc.blogadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
